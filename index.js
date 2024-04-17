f3c08619a7ce729b954186fe68ae7745a09120c1
JavieraGG2 <javiera.gutierrez.garcia20@gmail.com>
2024-04-17 12:00:33 -0400

    barra-Responsive

---
+++ index.js
@@ -0,0 +1,24 @@
+const navToggle=document.querySelector(".nav-toggle");
+const navMenu= document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click",()=>{
+    navMenu.classList.toggle("nav-menu_visible");
+
+    if (nav-menu.classList.contains("nav-menu_visible")){
+        navToggle.setAttribute("aria-label","Cerrar menú");
+        
+    }else{
+        navToggle.setAttribute("aria-label","Abrir menú");
+    }
+
+});
+
+
+
+
+
+  
+
+
+
+
